a0f7e22f8582c66f30f030705cc4f36d41541ec1
AHTOH <IlinAN89@yandex.ru>
2022-12-22 10:31:04 +1000

    HomeWork_8

---
+++ HomeWork_8/src/main/java/Controller.java
@@ -0,0 +1,56 @@
+import enums.Functionality;
+import enums.Periods;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Controller {
+
+    WeatherProvider weatherProvider = new AccuWeatherProvider();
+    Map<Integer, Functionality> variantResult = new HashMap();
+
+    public Controller() {
+        variantResult.put(1, Functionality.GET_CURRENT_WEATHER);
+        variantResult.put(2, Functionality.GET_WEATHER_IN_NEXT_5_DAYS);
+        variantResult.put(3, Functionality.GET_WEATHER_FROM_BD);
+    }
+
+    public void onUserInput(String input) throws IOException, SQLException {
+        int command = Integer.parseInt(input);
+        if (!variantResult.containsKey(command)) {
+            throw new IOException("There is no command for command-key " + command);
+        }
+
+        switch (variantResult.get(command)) {
+            case GET_CURRENT_WEATHER:
+                getCurrentWeather();
+                break;
+            case GET_WEATHER_IN_NEXT_5_DAYS:
+                getWeatherIn5Days();
+                break;
+            case GET_WEATHER_FROM_BD:
+                readWeatherOneDayFromDB();
+                break;
+        }
+    }
+
+    public void getCurrentWeather () throws IOException, SQLException {
+        weatherProvider.getWeather(Periods.NOW);
+    }
+
+    public void getWeatherIn5Days () throws IOException, SQLException {
+        weatherProvider.getWeather(Periods.FIVE_DAYS);
+    }
+
+    public void readWeatherOneDayFromDB() throws IOException, SQLException {
+        String selectedCity = ApplicationGlobalState.getInstance().getSelectedCity();
+        weatherProvider.readWeatherDayFromDB(selectedCity);
+    }
+
+    /*public void readWeatherFiveDayFromDB() throws IOException, SQLException {
+        String selectedCity = ApplicationGlobalState.getInstance().getSelectedCity();
+        weatherProvider.readWeatherFiveDayFromDB(selectedCity);*/
+
+}
---
+++ HomeWork_8/src/main/java/WeatherMain.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class WeatherMain {
+    public static void main(String[] args) throws SQLException, IOException {
+        DatabaseRepositorySQLitelmpl databaseRepositorySQLite = new DatabaseRepositorySQLitelmpl();
+        AccuWeatherProvider accuWeatherProvider = new AccuWeatherProvider();
+
+        databaseRepositorySQLite.getConnection();
+        // databaseRepositorySQLite.performDropTable();
+        databaseRepositorySQLite.createTableIfNotExists();
+
+        UserInterface userInterface = new UserInterface();
+
+        userInterface.runApplication();
+
+        accuWeatherProvider.readWeatherDayFromDB(accuWeatherProvider.detectCityKey());
+    }
+}
